9c59a75485c62e55b98eae7340915d8ec5e86847
pumperinho <pumperinho19@gmail.com>
2022-04-08 10:26:53 +0300

    add cosmic script

---
+++ cosmic/cosmic.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+cd $HOME
+wget https://raw.githubusercontent.com/pumperinho/scripts/main/logo.sh
+chmod +x logo.sh
+./logo.sh
+
+# update
+sudo apt update && sudo apt upgrade -y
+
+# packages
+sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
+
+sudo apt install -y uidmap dbus-user-session
+
+# GO
+sudo apt update  
+sudo apt install build-essential jq wget git -y
+
+wget https://dl.google.com/go/go1.17.1.linux-amd64.tar.gz
+tar -xvf go1.17.1.linux-amd64.tar.gz
+sudo mv go /usr/local
+Now add go to your bashrc -
+
+echo "" >> ~/.bashrc
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.bashrc
+
+source ~/.bashrc
+
+# Starport
+curl https://get.starport.network/starport! | bash
+curl https://get.starport.network/starport | bash
+sudo mv starport /usr/local/bin/
+
+# Clone the Repo
+git clone https://github.com/cosmic-horizon/coho.git
+
+# Install CoHo
+cd ~/coho
+starport chain build
+
+cd ~
+
+COSMIC_CHAIN="darkenergy-1"
+read -p "YOUR_MONIKER_NAME: " COSMIC_MONIKER
+read -p "YOUR_WALLET_NAME: " COSMIC_WALLET
+
+echo 'export COSMIC_CHAIN='${COSMIC_CHAIN} >> $HOME/.bash_profile
+echo 'export COSMIC_MONIKER='${COSMIC_MONIKER} >> $HOME/.bash_profile
+echo 'export COSMIC_WALLET='${COSMIC_WALLET} >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+cohod init ${COSMIC_MONIKER} --chain-id ${COSMIC_CHAIN}
+cohod config chain-id $COSMIC_CHAIN
+
+archwayd unsafe-reset-all
+
+cohod config chain-id ${COSMIC_CHAIN}
+cohod config keyring-backend test
+
+cohod keys add ${COSMIC_WALLET} &> mnemonic.txt
